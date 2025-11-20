aead887454c7dc76214af66f9fb345dbe6f012ee
Faiyad <faiyad@allinonedigitalx.info>
2025-11-20 19:54:38 +0600

    hotjar

---
+++ hotjar.js
@@ -0,0 +1,13 @@
+// Hotjar Integration Script
+
+(function(h,o,t,j,a,r){
+  h.hj = h.hj || function(){(h.hj.q = h.hj.q || []).push(arguments)};
+  h._hjSettings = h._hjSettings || {hjid: {{YOUR_HOTJAR_ID}}, hjsv: 6};
+  a = o.getElementsByTagName('head')[0];
+  r = o.createElement('script');
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+
+// Replace {{YOUR_HOTJAR_ID}} with your Hotjar Site ID
